6ded9cc231d3c8632c131f18c95313cda8ae1036
ashuralyk <ashuralyk@live.com>
2024-12-05 17:37:50 +0800

    chore: fix typo

--- packages/spore/src/__examples__/searchClusters.test.ts
+++ packages/spore/src/__examples__/searchClusters.test.ts
@@ -1,18 +1,18 @@
 import { ccc } from "@ckb-ccc/core";
 import { findSporeClustersBySigner } from "../cluster";
 
-describe("searchSpores [testnet]", () => {
+describe("searchClusters [testnet]", () => {
   expect(process.env.PRIVATE_KEY).toBeDefined();
 
-  it("should search multiple Spore cells under private key", async () => {
+  it("should search multiple Cluster cells under private key", async () => {
     const client = new ccc.ClientPublicTestnet();
     const signer = new ccc.SignerCkbPrivateKey(
       client,
       process.env.PRIVATE_KEY!,
     );
 
-    // Search Spore cells
-    for await (const cluster of findSporeClustersBySigner({ signer })) {
+    // Search Cluster cells
+    for await (const cluster of findSporeClustersBySigner({ signer, order: "desc" })) {
       console.log(cluster);
     }
   }, 60000);
