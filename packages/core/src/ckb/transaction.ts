2f7fcc2670633b7d8e47ace0ada76662ec187ffa
Hanssen0 <hanssen0@hanssen0.com>
2025-06-15 02:08:45 +0800

    feat(udt): balance related util methods

--- packages/core/src/ckb/transaction.ts
+++ packages/core/src/ckb/transaction.ts
@@ -1668,6 +1668,12 @@ export class Transaction extends mol.Entity.Base<
     );
   }
 
+  /**
+   * @deprecated Use `Udt.getInputsBalance` from `@ckb-ccc/udt` instead
+   * @param client
+   * @param type
+   * @returns
+   */
   async getInputsUdtBalance(client: Client, type: ScriptLike): Promise<Num> {
     return reduceAsync(
       this.inputs,
@@ -1683,6 +1689,11 @@ export class Transaction extends mol.Entity.Base<
     );
   }
 
+  /**
+   * @deprecated Use `Udt.getOutputsBalance` from `@ckb-ccc/udt` instead
+   * @param type
+   * @returns
+   */
   getOutputsUdtBalance(type: ScriptLike): Num {
     return this.outputs.reduce((acc, output, i) => {
       if (!output.type?.eq(type)) {
@@ -1800,6 +1811,7 @@ export class Transaction extends mol.Entity.Base<
    * This method succeeds only if enough balance is collected.
    *
    * It will try to collect at least two inputs, even when the first input already contains enough balance, to avoid extra occupation fees introduced by the change cell. An edge case: If the first cell has the same amount as the output, a new cell is not needed.
+   * @deprecated Use `Udt.completeInputs` from `@ckb-ccc/udt` instead
    * @param from - The signer to complete the inputs.
    * @param type - The type script of the UDT.
    * @param balanceTweak - The tweak of the balance.
