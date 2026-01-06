b2dce2e4d69e9f7320187a781d949cc057f25e4d
Hanssen0 <hanssen0@hanssen0.com>
2026-01-07 00:00:37 +0800

    feat(type-id): add type-id package for Type ID operations

--- packages/shell/src/barrel.ts
+++ packages/shell/src/barrel.ts
@@ -1,4 +1,5 @@
 export * from "@ckb-ccc/core/barrel";
 export { spore } from "@ckb-ccc/spore";
 export { ssri } from "@ckb-ccc/ssri";
+export { typeId } from "@ckb-ccc/type-id";
 export { udt } from "@ckb-ccc/udt";
