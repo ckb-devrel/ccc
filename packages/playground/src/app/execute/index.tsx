0090d1239e1a2fd0311ef3233ba3bcb0fe2647ae
fgh <fghdotio@gmail.com>
2026-01-27 23:59:31 -0500

    feat(playground): add bitcoin support for PSBT operations

--- packages/playground/src/app/execute/index.tsx
+++ packages/playground/src/app/execute/index.tsx
@@ -1,5 +1,15 @@
+import * as ecc from "@bitcoinerlab/secp256k1";
+import * as cccLib from "@ckb-ccc/ccc";
+import * as cccAdvancedLib from "@ckb-ccc/ccc/advanced";
 import { ccc } from "@ckb-ccc/connector-react";
+import * as dobRenderLib from "@nervina-labs/dob-render";
+import * as bitcoin from "bitcoinjs-lib";
 import * as React from "react";
+
+// Initialize the ECC library for bitcoinjs-lib to support Schnorr signatures (Taproot).
+// This must be done once globally before any PSBT operations.
+bitcoin.initEccLib(ecc);
+
 import ts from "typescript";
 import { formatTimestamp } from "../utils";
 import { vlqDecode } from "./vlq";
