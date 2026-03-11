779be3376b180a0d477912321d0b385051d6baba
fgh <fghdotio@gmail.com>
2026-03-11 02:12:51 -0400

    feat: Add RGB++ Spore and UDT examples

---
+++ packages/rgbpp/src/examples/spore/4-spore-btc-to-ckb.ts
@@ -0,0 +1,79 @@
+import { spore } from "@ckb-ccc/spore";
+
+import "../common/load-env.js";
+
+import { initializeRgbppEnv } from "../common/env.js";
+
+import { RgbppTxLogger } from "../common/logger.js";
+
+async function btcSporeToCkb({
+  ckbAddress,
+  sporeTypeArgs,
+}: {
+  ckbAddress: string;
+  sporeTypeArgs: string;
+}) {
+  const {
+    rgbppBtcWallet,
+    rgbppUdtClient,
+    utxoBasedAccountAddress,
+    ckbRgbppUnlockSigner,
+    ckbClient,
+    ckbSigner,
+  } = await initializeRgbppEnv();
+
+  const { tx: ckbPartialTx } = await spore.transferSpore({
+    signer: ckbSigner,
+    id: sporeTypeArgs,
+    to: await rgbppUdtClient.buildBtcTimeLockScript(ckbAddress),
+  });
+
+  const { psbt, indexedCkbPartialTx } = await rgbppBtcWallet.buildPsbt({
+    ckbPartialTx,
+    ckbClient,
+    rgbppUdtClient,
+    btcChangeAddress: utxoBasedAccountAddress,
+    receiverBtcAddresses: [],
+    feeRate: 28,
+  });
+  logger.logCkbTx("indexedCkbPartialTx", indexedCkbPartialTx);
+
+  const btcTxId = await rgbppBtcWallet.signAndBroadcast(psbt);
+  logger.add("btcTxId", btcTxId, true);
+
+  const ckbPartialTxInjected = await rgbppUdtClient.injectTxIdToRgbppCkbTx(
+    indexedCkbPartialTx,
+    btcTxId,
+  );
+  const rgbppSignedCkbTx =
+    await ckbRgbppUnlockSigner.signTransaction(ckbPartialTxInjected);
+
+  await rgbppSignedCkbTx.completeFeeBy(ckbSigner);
+  const ckbFinalTx = await ckbSigner.signTransaction(rgbppSignedCkbTx);
+  const txHash = await ckbSigner.client.sendTransaction(ckbFinalTx);
+  await ckbRgbppUnlockSigner.client.waitTransaction(txHash);
+  logger.add("ckbTxId", txHash, true);
+}
+
+const logger = new RgbppTxLogger({ opType: "spore-btc-to-ckb" });
+
+btcSporeToCkb({
+  ckbAddress:
+    "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqfpu7pwavwf3yang8khrsklumayj6nyxhqpmh7fq",
+  sporeTypeArgs:
+    "0x8ce8307ac273c6e5548bd1a5dbf6596aab5dd5e75259a092b5461d3dba1c34bf",
+})
+  .then(() => {
+    logger.saveOnSuccess();
+    process.exit(0);
+  })
+  .catch((e) => {
+    const error = e instanceof Error ? e : new Error(String(e));
+    console.log(error.message);
+    logger.saveOnError(error);
+    process.exit(1);
+  });
+
+/* 
+pnpm tsx packages/rgbpp/src/examples/spore/4-spore-btc-to-ckb.ts
+*/
