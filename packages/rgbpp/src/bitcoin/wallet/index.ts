02ee42c31d5be0ef0341f86b3387cb1acf6d11ae
fgh <fghdotio@gmail.com>
2026-03-10 23:47:13 -0400

    refactor: Extract Bitcoin transaction building, fee estimation, and API client into dedicated classes

--- packages/rgbpp/src/bitcoin/wallet/index.ts
+++ packages/rgbpp/src/bitcoin/wallet/index.ts
@@ -1,5 +1,7 @@
 export * from "./account.js";
 export * from "./base.js";
 export * from "./browser.js";
+export * from "./fee-estimator.js";
 export * from "./private-key.js";
 export * from "./public-key.js";
+export * from "./tx-builder.js";
