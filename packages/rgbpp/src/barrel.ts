572f82ed014a8ab3d7b8c8679d85d26d2f49955e
fgh <fghdotio@gmail.com>
2026-05-11 21:02:33 -0400

    fix(rgbpp): complete barrel exports and move @ckb-ccc/spore to dependencies

--- packages/rgbpp/src/barrel.ts
+++ packages/rgbpp/src/barrel.ts
@@ -1,3 +1,7 @@
 export * from "./bitcoin/index.js";
+export * from "./data-source/index.js";
 export * from "./error.js";
+export * from "./script/index.js";
+export * from "./signer/index.js";
 export * from "./udt/index.js";
+export * from "./utils/index.js";
