97604a698910093b49128d329c856377c167b87c
aabbcc <iyranly@163.com>
2026-05-20 22:08:15 +0800

    fix(docs): Type error: Object literal may only specify known properties, and 'cn' does not exist

--- packages/ccc-docs/app/api/search/route.ts
+++ packages/ccc-docs/app/api/search/route.ts
@@ -1,6 +1,6 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
-import { createTokenizer } from '@orama/tokenizers/mandarin';
+//import { createTokenizer } from '@orama/tokenizers/mandarin';
 
 
 // i18n-aware search: Orama supports per-locale indexes.
@@ -8,11 +8,11 @@ import { createTokenizer } from '@orama/tokenizers/mandarin';
 export const { GET } = createFromSource(source, {
   localeMap: {
     en: { language: 'english' },
-    cn: { 
-      // Don't specify language when using custom tokenizer
-      components: {
-        tokenizer: createTokenizer(),
-      },
-    },
+    //cn: { 
+    //  // Don't specify language when using custom tokenizer
+    //  components: {
+    //    tokenizer: createTokenizer(),
+    //  },
+    //},
   },
 });
