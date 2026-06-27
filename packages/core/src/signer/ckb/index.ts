9beee4d7f39e4b4b19919c5feb86e1c8be0e089d
Hanssen0 <0@hanssen0.com>
2026-06-28 07:14:41 +0800

    feat(core): `signMessageSecp256k1`

--- packages/core/src/signer/ckb/index.ts
+++ packages/core/src/signer/ckb/index.ts
@@ -1,5 +1,5 @@
+export * from "./secp256k1Signing.js";
 export * from "./signerCkbPrivateKey.js";
 export * from "./signerCkbPublicKey.js";
 export * from "./signerCkbScriptReadonly.js";
-export * from "./verifyCkbSecp256k1.js";
 export * from "./verifyJoyId.js";
