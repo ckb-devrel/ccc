bfe4d203e99de01ff0bce579524c98c1be16dc1e
Hanssen0 <0@hanssen0.com>
2026-07-21 02:16:08 +0800

    feat(core): `SignerCkbAlwaysSuccess`

--- packages/core/src/signer/ckb/index.ts
+++ packages/core/src/signer/ckb/index.ts
@@ -1,4 +1,5 @@
 export * from "./secp256k1Signing.js";
+export * from "./signerCkbAlwaysSuccess.js";
 export * from "./signerCkbPrivateKey.js";
 export * from "./signerCkbPublicKey.js";
 export * from "./signerCkbScriptReadonly.js";
